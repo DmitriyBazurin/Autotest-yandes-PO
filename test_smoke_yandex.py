5786c30168145d787500aae5d4adb95eed4508db
DmitriyBazurin <bazurin.dmitriy@mail.ru>
2022-04-04 16:35:14 +0300

    Update test_smoke_yandex.py

--- test_smoke_yandex.py
+++ test_smoke_yandex.py
@@ -26,11 +26,13 @@ def test_yandex_pictures(browser):
     page.click_on_the_1st_pictures_img()
 
     page.click_on_the_1st_picture_yandex()
+    time.sleep(1)
     pic1 = page.get_current_url()
 
     page.click_on_the_next_picture_yandex()
 
     page.click_on_the_prev_picture_yandex()
+    time.sleep(1)
     pic2 = page.get_current_url()
     assert pic1 == pic2
 
