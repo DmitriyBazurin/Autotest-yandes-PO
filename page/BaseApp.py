be77993b48c3bb17353c21b4959f45bb4faa39c5
DmitriyBazurin <bazurin.dmitriy@mail.ru>
2022-04-04 16:28:19 +0300

    update test

--- page/BaseApp.py
+++ page/BaseApp.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class BasePage:
+class BasePage(object):
 
     def __init__(self, driver):
         self.driver = driver
@@ -14,3 +14,6 @@ class BasePage:
 
     def go_to_site(self):
         return self.driver.get(self.base_url)
+
+    def get_current_url(self):
+        return self.driver.current_url
---
+++ test_smoke_yandex.py
@@ -0,0 +1,41 @@
+from page.YandexPages import YandexMethods
+from selenium import webdriver
+import time
+
+
+
+def test_yandex_search(browser):
+    page = YandexMethods(browser)
+    page.go_to_site()
+    page.enter_word("Тензор")
+    page.click_on_the_search_button()
+    time.sleep(1)
+
+def test_yandex_pictures(browser):
+    driver = webdriver.Chrome
+    page = YandexMethods(browser)
+    page.go_to_site()
+    page.click_on_the_pictures_button()
+
+    tabs = browser.window_handles
+    browser.switch_to.window(tabs[1])
+    get_url = page.get_current_url()
+    assert 'yandex.ru/images' in get_url
+
+
+    page.click_on_the_1st_pictures_img()
+
+    page.click_on_the_1st_picture_yandex()
+    pic1 = page.get_current_url()
+
+    page.click_on_the_next_picture_yandex()
+
+    page.click_on_the_prev_picture_yandex()
+    pic2 = page.get_current_url()
+    assert pic1 == pic2
+
+
+
+
+
+    
